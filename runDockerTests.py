d048040638e54289f9613c7ab55197b13abbce5e
R Y <rebecca@ryates.me.uk>
2014-10-05 01:43:18 +0100

    Running each test in its own container

--- runDockerTests.py
+++ runDockerTests.py
@@ -7,6 +7,7 @@ Some assumptions:
     * the container base image has a straight copy of all the local tests
     * all tests are in a single test directory (no support for recursion here)
     * tests all succeed (not examining test results or restarting containers on failure)
+    * containers exit cleanly
 """
 
 
@@ -15,9 +16,12 @@ import tarfile
 import os
 import json
 import inspect
+import time
 
 import docker
 import nose
+
+start_time = time.time()
 
 client = docker.Client(base_url='unix://var/run/docker.sock')
 JSON_RESULTS_FILENAME = "result.json"
@@ -25,19 +29,29 @@ TESTS_DIRECTORY = "tests"
 IMAGE_NAME = "longtest"
 JSON_RESULTS_PATH = os.path.join(TESTS_DIRECTORY, JSON_RESULTS_FILENAME)
 
-#Use nosetests to find files containing tests
-test_files = []
+#Use nosetests to find individual test cases
+test_cases = []
 nose_test = nose.loader.TestLoader()
-for test_suite in nose_test.loadTestsFromDir(TESTS_DIRECTORY):
-    test_filename = inspect.getfile(test_suite.context)
-    test_files.append(os.path.relpath(test_filename))
+for test_file in nose_test.loadTestsFromDir(TESTS_DIRECTORY):
+    #test_filename = inspect.getfile(test_file.context)
+    for test_suite in test_file:
+        #print(test_suite.context)
+        for test_case in test_suite:
+            address = test_case.address()
+            test_filename = os.path.relpath(address[0])
+            test_name = address[2]
+    
+    
+            test_cases.append("%s:%s" % (test_filename, test_name))
+
 
 #Build a container to run each test file
 containers = []
-for test_file in test_files:
-    command = "nosetests --with-json --json-file='%s' %s" % (JSON_RESULTS_PATH, test_file)
+for test_case in test_cases:
+    command = "nosetests --with-json --json-file='%s' %s" % (JSON_RESULTS_PATH, test_case)
     container = client.create_container(IMAGE_NAME, command)
     containers.append(container)
+
 
 #Start containers
 print("Starting %d containers" % len(containers))
@@ -58,13 +72,18 @@ def collect_results(client, containers):
         result_file = tar.extractfile(os.path.basename(JSON_RESULTS_FILENAME))
         for line in result_file:
             #is it safe to assume ascii here?
-            print(json.loads(line.decode("ascii")))
+            #print(json.loads(line.decode("ascii")))
             results.extend(json.loads(line.decode("ascii"))['results'])
     return results
 
 #Collect aggregated test results
 results = collect_results(client, containers)
 
+#Clean up containers
+for container in containers:
+    client.remove_container(container)
+
 #Have a look
 print([result['time'] for result in results])
 
+print("--- %s seconds ---" % (time.time() - start_time))
